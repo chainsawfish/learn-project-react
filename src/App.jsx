4194690b1c7b0c686bc7d2981845cfcb1c3fc710
Denis <chainsawfish@gmail.com>
2024-01-12 19:34:04 +0700

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-const cardStyle = {
-  border: "0px solid black",
-  padding: "10px",
-  margin: "10px",
-  borderRadius: "10px",
-  boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.7)",
-};
-
-const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
-
 function App() {
-  console.log("render");
-  const [first, setfirst] = useState(null);
-
-  useEffect(() => {
-    axios.get(URL_POSTS).then((response) => {
-      setfirst(response.data);
-    });
-  }, []);
-
-  return (
-    <>
-      {first &&
-        first.map((post) => {
-          return (
-            <div key={post.id} style={cardStyle}>
-              <div>{post.title}</div>
-              <div>----------</div>
-            </div>
-          );
-        })}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
